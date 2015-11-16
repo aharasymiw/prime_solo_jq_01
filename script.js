a58963638c7ee7a6341bfe0b36f6d9090cec2b02
Andrew Harasymiw <aharasymiw@gmail.com>
2015-11-15 19:35:05 -0600

    Resorting employeeArray for removed employee works, cosmetic chages to css

--- script.js
+++ script.js
@@ -11,21 +11,54 @@ function Employee(firstName, lastName, empNum, title, reviewScore, salary) {
 // Global variable declarations, and default initializations
 var employeeArray = []; // Each element represents one employee as an Employee object
 
-// MAKE STURE TO DELETE THIS BLOCK!!!!!
-// Added for testing
-employeeArray.push(new Employee('Andrew', 'Harasymiw', 42, 'Cool Guy', 5, 65000)); // Add the new Employee instance to the array of employees
-console.log(employeeArray);
-employeeArray.push(new Employee('Lauren', 'Jelenchick', 1, 'Love of Life', 5, 25000)); // Add the new Employee instance to the array of employees
-console.log(employeeArray);
-employeeArray.push(new Employee('Derek', 'Roche', 99, 'Cool Guy', 3, 55000)); // Add the new Employee instance to the array of employees
-console.log(employeeArray);
-employeeArray.push(new Employee('Gwen', 'Paul', 83, 'Cool Girl', 4, 60000)); // Add the new Employee instance to the array of employees
-console.log(employeeArray);
-employeeArray.push(new Employee('John', 'Crimmings', 3, 'Cool Guy', 4, 70000)); // Add the new Employee instance to the array of employees
-console.log(employeeArray);
+// MAKE STURE TO DELETE THIS BLOCK!!!!! (It's For testing, yo)
+ // Add the new Employee instances to the array of employees
+employeeArray.push(new Employee('Andrew', 'Harasymiw', 42, 'Cool Guy', 5, 65000));
+employeeArray.push(new Employee('Lauren', 'Jelenchick', 1, 'Love of Life', 5, 25000));
+employeeArray.push(new Employee('Derek', 'Roche', 99, 'Cool Guy', 3, 55000));
+employeeArray.push(new Employee('Gwen', 'Paul', 83, 'Cool Girl', 4, 60000));
+employeeArray.push(new Employee('John', 'Crimmings', 3, 'Cool Guy', 4, 70000));
+
+// Print the entire employeeArray of 'test' names to the screen
 for (var i = 0; i < employeeArray.length; i++){
   drawEmployee(i); // Draw the most newly input employee
 }
+
+// A function to sort employeeArray[i], optionally takes a 'how to sort' pararameter 
+function sortEmployees(toSort, how){
+  var sorted = [];
+  
+  switch(how) {
+    /* // Optional implimentation for sorting by different property values
+    case how = 'firstName':
+      //code block
+      break;
+    case how = 'lastName':
+      //code block
+      break;
+    case how = 'empNum':
+      //code block
+      break;
+    case how = 'title':
+      //code block
+      break;
+    case how = 'reviewScore':
+      //code block
+      break;
+    case how = 'salary':
+      //code block
+      break;
+    */          
+    default:
+      for (var i = 0; i < toSort.length; i++){
+        if ((typeof toSort[i]) != 'undefined') {
+          sorted.push(toSort[i]);
+          sorted[i]
+        }
+      }
+  }
+  return sorted;      
+} 
 
 // A function to draw a an Employee object for employeeArray[i]
 function drawEmployee(i){
@@ -73,11 +106,12 @@ $(function() { // Wait until the document is loaded and ready
   $('#empList').on('click', 'button', function(){ // Check for a .remove Employee button click
     var employeePosition = $(this).attr('id'); // Stores the Employee's possition as the .class[i] of the .remove button
     
-    // Updates Employee's possition to [i], it's loctaion in employeeArray
-    employeePosition = employeePosition.charAt(employeePosition.length - 1); 
+    // Updates Employee's position to [i], it's loctaion in employeeArray
+    employeePosition = parseInt(employeePosition.slice(8, employeePosition.length)); // Extract the numeric portion of #employee[i]
     delete employeeArray[employeePosition]; // Remove the Employee from employeeArray
-    $(this).closest('ul').remove();
+    $(this).closest('ul').remove(); // Remove the Employee from <ol #empList>, ie: undraw the Employee
     
-    console.log(employeeArray);
+    // Remove the empty slot left by the deleted Employee, and update button #employee[i] for following employees     
+    employeeArray = sortEmployees(employeeArray);
   });
 });
