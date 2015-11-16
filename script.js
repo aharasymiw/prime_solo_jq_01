807a841fa676f6d811262709152c89d548730696
Andrew Harasymiw <aharasymiw@gmail.com>
2015-11-16 03:17:09 -0600

    Fully working, except the sort by name and its ugly

--- script.js
+++ script.js
@@ -1,45 +1,81 @@
 // Constructor for Employee object
-function Employee(firstName, lastName, empNum, title, reviewScore, salary) {
+function Employee(firstName, lastName, empNum, jobTitle, reviewScore, salary) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.empNum = empNum;
-  this.title = title;
-  this.reviewScore = reviewScore;
-  this.salary = salary;
+  this.empNum = parseInt(empNum);
+  this.jobTitle = jobTitle;
+  this.reviewScore = parseInt(reviewScore);
+  this.salary = parseInt(salary);
 }
 
 // Global variable declarations, and default initializations
 var employeeArray = []; // Each element represents one employee as an Employee object
+var employeeCost = 0;
 
-// MAKE STURE TO DELETE THIS BLOCK!!!!! (It's For testing, yo)
- // Add the new Employee instances to the array of employees
-employeeArray.push(new Employee('Andrew', 'Harasymiw', 42, 'Cool Guy', 5, 65000));
-employeeArray.push(new Employee('Lauren', 'Jelenchick', 1, 'Love of Life', 5, 25000));
-employeeArray.push(new Employee('Derek', 'Roche', 99, 'Cool Guy', 3, 55000));
-employeeArray.push(new Employee('Gwen', 'Paul', 83, 'Cool Girl', 4, 60000));
-employeeArray.push(new Employee('John', 'Crimmings', 3, 'Cool Guy', 4, 70000));
+function updateEmployeeCost(){
+  employeeCost = 0;
+  for (var i = 0; i < employeeArray.length; i++){
+    employeeCost += employeeArray[i].salary;
+  }
+}
 
-// Print the entire employeeArray of 'test' names to the screen
-for (var i = 0; i < employeeArray.length; i++){
-  drawEmployee(i); // Draw the most newly input employee
+// A function to generate a random employee
+function generateEmployee(){
+  var firstName = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5); // Generate random firstName
+  var lastName = Math.random().toString(36).replace(/[^a-z]+/g, '') // Generate random lastName
+  var empNum = Math.floor((Math.random() * 1000) + 1); // Generate random empNum
+  var jobTitle = 'Randomly Generated'; // Generate static jobTitle
+  var reviewScore = Math.floor((Math.random() * 5) + 1); // Generate random reviewScore(1-5)
+  var salary = Math.floor((Math.random() * 100000) + 1); // Generate random salary
+  
+  // Create random Employee object from generated values, and push it to the employeeArrray
+  employeeArray.push(new Employee(firstName, lastName, empNum, jobTitle, reviewScore, salary));
+  updateEmployeeCost(); // Recalculate the total labor expence
+  redrawArray(); // Redraw the employeeArray
 }
 
 // A function to sort employeeArray[i], optionally takes a 'how to sort' pararameter 
-function sortEmployees(toSort, how){
+function sortEmployees(how){
   var sorted = [];
+  var tmp = 0;
+  var tmp2 = 0;
   
   switch(how) {
-    /* // Optional implimentation for sorting by different property values
-    case how = 'firstName':
-      //code block
+    case how = 'firstName': // Sort employees by First name   
+      for (var i = 0; i < employeeArray.length - 1; i++){
+        tmp = i;
+        for (var j = i + 1; j < employeeArray.length; j++){
+          if (employeeArray[j].firstName < employeeArray[i].firstName){
+            tmp = j;
+          }
+        }
+        if(tmp != i){
+          tmp2 = employeeArray[tmp];
+          employeeArray[tmp] = employeeArray[i];
+          employeeArray[i] = tmp2;
+        }
+      }           
       break;
-    case how = 'lastName':
-      //code block
+    case how = 'lastName': // Sort employees by Last name
+       for (var i = 0; i < employeeArray.length - 1; i++){
+        tmp = i;
+        for (var j = i + 1; j < employeeArray.length; j++){
+          if (employeeArray[j].lastName < employeeArray[i].lastName){
+            tmp = j;
+          }
+        }
+        if(tmp != i){
+          tmp2 = employeeArray[tmp];
+          employeeArray[tmp] = employeeArray[i];
+          employeeArray[i] = tmp2;
+        }
+      }
       break;
+   /* Optional implimentation for sorting by different property values      
     case how = 'empNum':
       //code block
       break;
-    case how = 'title':
+    case how = 'jobTitle':
       //code block
       break;
     case how = 'reviewScore':
@@ -49,20 +85,19 @@ function sortEmployees(toSort, how){
       //code block
       break;
     */          
-    default:
-      for (var i = 0; i < toSort.length; i++){
-        if ((typeof toSort[i]) != 'undefined') {
-          sorted.push(toSort[i]);
-          sorted[i]
+    default: // Sort the employeeArray, keeping the current order, but removing 'undefined' (empty) spaces
+      for (var i = 0; i < employeeArray.length; i++){
+        if ((typeof employeeArray[i]) != 'undefined') {  // Check that the current element of employeeArray is not undefined
+          sorted.push(employeeArray[i]); // Push the current element of employeeArray to a sorted array
         }
       }
+      return sorted;
   }
-  return sorted;      
 } 
 
 // A function to draw a an Employee object for employeeArray[i]
 function drawEmployee(i){
-  var employee = employeeArray[i]; // 
+  var employee = employeeArray[i]; // Create local veriable to reference the Employee that is being drawn
   var $ul = $('<ul>'); // Create a <ul> element to hold the new employee
   var $button = $('<button>');
 
@@ -70,22 +105,60 @@ function drawEmployee(i){
   $ul.append($('<li>').text('First Name: ' + employee.firstName));
   $ul.append($('<li>').text('Last Name: ' + employee.lastName));
   $ul.append($('<li>').text('Employee #: ' + employee.empNum));
-  $ul.append($('<li>').text('Title: ' + employee.title));
-  $ul.append($('<li>').text('Review Score (1-5): ' + employee.reviewScore));
+  $ul.append($('<li>').text('Title: ' + employee.jobTitle));
+  // Enclose the numeric reviewScore in an <em> tag for CSS styling
+  $ul.append($('<li>').text('Review Score (1-5): ').append($('<em>').text(employee.reviewScore)));
   $ul.append($('<li>').text('Salary: $' + employee.salary));
   
   // Append a 'remove' button to the <ul> for the employee
   $ul.append($($button).text('Delete: ' + employee.firstName + ' ' + employee.lastName));
 
   $button.attr('id', 'employee' + i); // Add a class to the remove button based on the Employee's position in employeeArray, #employee[i]
-  $button.addClass('remove'); // Add a .remove class to the remove button 
+  $button.addClass('remove'); // Add a .remove class to the remove button
+  $button.addClass('button');
   $ul.addClass('review' + employee.reviewScore); // Add a class to the <ul> based on Employee reviewScore, .review(1-5)
   $('#empList').append($ul); // Add the <ul> for the employee to <ol #empList> of employees
 }
 
-// Add new employee
+// A function to redraw the employeeArray
+function redrawArray(){
+  $('ol > ul').remove(); // Remove all Employee's from <ol #empList>, ie: undraw the Employee
+  $('aside > h2').remove();
+  $('aside').append($('<h2>').text('Total Annual Labor Cost: ' + employeeCost));
+  
+  // Reprint the entire employeeArray of 'test' names to the screen
+  for (var i = 0; i < employeeArray.length; i++){
+    drawEmployee(i); // Draw Employee i from employeeArray[i]  
+  }
+}
+
+// Add a new random employee when button is clicked
 $(function() { // Wait until the document is loaded and ready
-  $('#empForm').on('submit', function(event){ // Check for a form submission
+  $('#random').click(function(event){ // listen for 'click' event on #random button
+    generateEmployee();
+  });
+});
+
+// Sort Employees by First Name when button is clicked
+$(function() { // Wait until the document is loaded and ready
+  $('#firstName').click(function(event){ // listen for 'click' event on #random button
+    sortEmployees('firstName'); // Sort employeeArray by firstName
+    redrawArray(); // Redraw the employeeArray
+  });
+});
+
+// Sort Employees by Last Name when button is clicked
+$(function() { // Wait until the document is loaded and ready
+  $('#lastName').click(function(event){ // listen for 'click' event on #random button
+    sortEmployees('lastName') // Sort employeeArray by lastName
+    redrawArray(); // Redraw the employeeArray  
+  });
+});
+
+
+// Add a new employee from the form when button is clicked
+$(function() { // Wait until the document is loaded and ready
+  $('#empForm').on('submit', function(event){ // Listen for 'submit' event on #empForm form
   
     var arrayEmployee = []; // Holds an new employee as an array, before it is turned into an Employee object
     var newEmployee = {}; // A new Employee object, to be added to employeeArray
@@ -97,7 +170,8 @@ $(function() { // Wait until the document is loaded and ready
     // Convert a newEmployee array into a new Employee object
     newEmployee = new Employee(arrayEmployee[0].value, arrayEmployee[1].value, arrayEmployee[2].value, arrayEmployee[3].value, arrayEmployee[4].value, arrayEmployee[5].value);
     employeeArray.push(newEmployee); // Add the new Employee instance to the array of employees
-    drawEmployee(employeeArray.length - 1); // Draw the most newly input employee
+    updateEmployeeCost(); // Recalculate the total labor expence
+    redrawArray(); // Redraw the employeeArray
   })
 });
 
@@ -109,9 +183,10 @@ $(function() { // Wait until the document is loaded and ready
     // Updates Employee's position to [i], it's loctaion in employeeArray
     employeePosition = parseInt(employeePosition.slice(8, employeePosition.length)); // Extract the numeric portion of #employee[i]
     delete employeeArray[employeePosition]; // Remove the Employee from employeeArray
-    $(this).closest('ul').remove(); // Remove the Employee from <ol #empList>, ie: undraw the Employee
     
     // Remove the empty slot left by the deleted Employee, and update button #employee[i] for following employees     
-    employeeArray = sortEmployees(employeeArray);
+    employeeArray = sortEmployees();
+    updateEmployeeCost(); // Recalculate the total labor expence
+    redrawArray(); // Redraw the employeeArray
   });
 });
